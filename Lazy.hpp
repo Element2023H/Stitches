6b3426248f47a784c5e84c0cd23ec80d7b1f66ff
jacky <lzty.jw@gmail.com>
2025-05-29 18:05:44 +0800

    提升LazyInstance的内存安全性

--- Lazy.hpp
+++ Lazy.hpp
@@ -2,14 +2,31 @@
 #include "Once.hpp"
 #include "New.hpp"
 
+constexpr ULONG LAZY_INSTANCE_MEM = 'mmyL';
 
-const ULONG LAZY_INSTANCE_MEM = 'mmyL';
+// stolen from STL
+// keep these type traits available until this project supports STL
+namespace traits {
+	template <bool _Test, class _Ty = void>
+	struct enable_if {}; // no member "type" when !_Test
+
+	template <class _Ty>
+	struct enable_if<true, _Ty> { // type is _Ty for _Test
+		using type = _Ty;
+	};
+
+	template <bool _Test, class _Ty = void>
+	using enable_if_t = typename enable_if<_Test, _Ty>::type;
+
+	template <class _Ty>
+	constexpr bool is_default_constructable_v = __is_constructible(_Ty);
+}
 
 /// <summary>
 /// A Lazy instance model for static singleton types, it initialize T when it is first accessed
 /// </summary>
 /// <typeparam name="T"></typeparam>
-template <typename T>
+template <typename T, POOL_TYPE PoolType = NonPagedPoolNx>
 class LazyInstance
 {
 public:
@@ -41,6 +58,9 @@ public:
 
 	FORCEINLINE static void Dispose()
 	{
+		if (_Once.GetState() != state::Completed)
+			KeBugCheck(MEMORY_MANAGEMENT);
+
 		delete _Instance;
 		_Once.SetPoisoned();
 	}
@@ -56,32 +76,48 @@ public:
 	/// <returns></returns>
 	FORCEINLINE T* operator -> ()
 	{
-		_Once.CallOnceAndWait([this]() {
-			_Instance = this->ForceDefault();
-			});
-
-		return _Instance;
+		return get();
 	}
 
 	FORCEINLINE const T* operator -> () const
 	{
-		_Once.CallOnceAndWait([this]() {
-			_Instance = this->ForceDefault();
+		return get();
+	}
+
+	FORCEINLINE const T* get() const
+	{
+		return ForceDefault();
+	}
+
+	FORCEINLINE T* get()
+	{
+		return ForceDefault();
+	}
+
+	FORCEINLINE const T& operator * () const
+	{
+		return *this->get();
+	}
+
+	FORCEINLINE T& operator * ()
+	{
+		return *this->get();
+	}
+
+private:
+	template <typename = traits::enable_if_t<traits::is_default_constructable_v<T>>>
+	FORCEINLINE T* ForceDefault()
+	{
+		_Once.CallOnceAndWait([]() {
+			return new(PoolType) T;
 			});
 
 		return _Instance;
 	}
-
-private:
-	FORCEINLINE T* ForceDefault()
-	{
-		//return reinterpret_cast<T*>(ExAllocatePoolWithTag(NonPagedPoolNx, sizeof(T), LAZY_INSTANCE_MEM));
-		return new(NonPagedPoolNx) T;
-	}
 };
 
-template <typename T>
-T* LazyInstance<T>::_Instance;
+template <typename T, POOL_TYPE PoolType>
+T* LazyInstance<T, PoolType>::_Instance;
 
-template <typename T>
-Once LazyInstance<T>::_Once;
+template <typename T, POOL_TYPE PoolType>
+Once LazyInstance<T, PoolType>::_Once;
